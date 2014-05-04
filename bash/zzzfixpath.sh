76c98d9eb0ec1cfe471cf0e07ea92758fc47a6cb
Hal Spitz (IC) <hspitz@okl3540-hspitz.gateway.2wire.net>
2014-05-04 13:51:02 -0700

    a little cleanup and fine tuning

--- bash/zzzfixpath.sh
+++ bash/zzzfixpath.sh
@@ -1,4 +1,5 @@
 # this could be done a LOT more cleverly
+# Make RVM happy by putting it's path requirements up in the front
 if [[ -d $HOME/.rvm && -d $GEM_HOME && -d $MY_RUBY_HOME ]]; then
   PATH=$GEM_HOME/bin:$MY_RUBY_HOME/bin:$HOME/.rvm/bin:$PATH
 fi
