20985d3449a258058a03e5dfbee420f23c8c3f97
Hal M. Spitz <hal.spitz@gmail.com>
2016-07-11 17:35:38 -0700

    Adds db.sh and updates bash_completion.sh
    db.sh: aliases for starting, stopping, and getting the status of postgresql
    bash_completion.sh: stuff to make bash work better

---
+++ bash/Alias/db.sh
@@ -0,0 +1,4 @@
+# alias pstart="postgres start -D /usr/local/var/postgres"
+alias pstart="pg_ctl start -D /usr/local/var/postgres"
+alias pstop="pg_ctl stop -D /usr/local/var/postgres"
+alias pstatus="pg_ctl status -D /usr/local/var/postgres"
--- bash/Completion/homebrew.bash_completion.sh
+++ bash/Completion/homebrew.bash_completion.sh
@@ -1,4 +1,4 @@
 if [ -f `brew --prefix`/etc/bash_completion ]; then
   . `brew --prefix`/etc/bash_completion
 fi
-source `brew --prefix`/Library/Contributions/brew_bash_completion.sh
+# source `brew --prefix`/Library/Contributions/brew_bash_completion.sh
