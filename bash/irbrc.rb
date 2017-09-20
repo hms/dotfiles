2551aa68b5dc742b60d42a554106193e1160ba52
Hal M. Spitz <hal.spitz@gmail.com>
2017-09-20 16:03:52 -0700

    Added support for AwesomePrint

--- bash/irbrc.rb
+++ bash/irbrc.rb
@@ -7,30 +7,25 @@ require 'rubygems' rescue nil
 # install helpful gems for every irb session
 #
 #############################################
-begin
-  require 'spirit_hands'
-  SpiritHands.melody!
+if defined?(Hirb)
+  Hirb.enable
 
-  if defined?(Hirb)
-    def resize
-      return 'Hirb not loaded' unless defined?(Hirb)
+  def resize
+    return 'Hirb not loaded' unless defined?(Hirb)
 
-      width = `tput cols`.to_i
-      Hirb.enable(width: width)
-      'Resized';
-    end
-
-    def wide
-      return 'Hirb not loaded' unless defined?(Hirb)
-      Hirb.enable(width: 250);
-    end
+    width = `tput cols`.to_i
+    Hirb.enable(width: width, pager: false)
+    'Resized';
   end
-rescue
-  if defined?(SpiritHands)
-    puts "Error initializing 'SpiritHands'"
-  else
-    puts 'SpiritHands Gem not available'
+
+  def wide
+    return 'Hirb not loaded' unless defined?(Hirb)
+    Hirb.enable(width: 250);
   end
+end
+
+if defined?(AwesomePrint)
+  AwesomePrint.irb!
 end
 
 #############################################
@@ -57,16 +52,9 @@ rescue
   puts 'DelayedJob support not available'
 end
 
-def zombie_away!
-  `ps -ef | grep fsevent_watch`.split("\n").each do |row|
-    `kill "#{row.split[1]}"`
-  end
-end
-
 def my_helpers
   puts 'delayed_info: information on the delayed_jobs table'
   puts 'resize: Hirb.enable(width: `tputs cols`)'
   puts 'wide: Hirb.enable(width: 250)'
-  puts 'zombie_away!: kills and ruby and rails processes that are running (not really a zombie killer just yet)'
 end
 
