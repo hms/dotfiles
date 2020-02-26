9adb3b859e6921c1fc534bbf1a582972df649cf6
Hal M. Spitz <hal.spitz@gmail.com>
2020-02-25 21:08:10 -0800

    update instantlabs aliases

--- bash/Alias/instantlabs.sh
+++ bash/Alias/instantlabs.sh
@@ -1,7 +1,10 @@
-alias idb="cd ~/Devel/go/src/github.com/instantlabs/ildb"
-alias idbb="cd ~/Devel/go/src/github.com/instantlabs/ildb/bbolt"
-alias ilib="cd ~/Devel/go/src/github.com/instantlabs/illib"
-alias icore="cd ~/Devel/go/src/github.com/instantlabs/ilcore"
+alias idb="cd ~/Devel/go/src/github.com/instantlabs/ildb; echo -ne '\033]1;ILDb\007' "
+alias idbb="cd ~/Devel/go/src/github.com/instantlabs/ildb/bbolt; echo -ne '\033]1;Bbolt\007' "
+alias ilib="cd ~/Devel/go/src/github.com/instantlabs/illib; echo -ne '\033]1;ILLib\007' "
+alias icore="cd ~/Devel/go/src/github.com/instantlabs/ilcore; echo -ne '\033]1;ILCore\007' "
+alias iclients="cd ~/Devel/go/src/github.com/instantlabs/clients; echo -ne '\033]1;IClients\007' "
+alias idbstress="cd ~/Devel/go/src/github.com/instantlabs/ildb/bbolt/cmd/stress_test; echo -ne '\033]1;StressTest\007' "
 
 
-alias start-ilcore="ccore; go build; env GO111MODULE=on go install github.com/instantlabs/ilcore/ilcore; ~/Devel/go/bin/ilcore"
+alias start-ilcore="icore; go build; env GO111MODULE=on go install github.com/instantlabs/ilcore/ilcore; ~/Devel/go/bin/ilcore"
+alias start-ildb="ilib; make run-ildb"
--- bash/term.sh
+++ bash/term.sh
@@ -1,2 +1,4 @@
+DISABLE_AUTO_TITLE="true"
+
 export HALF_COLUMNS=$(($COLUMNS/2))
 export HALF_LINES=$(($LINES/2))
