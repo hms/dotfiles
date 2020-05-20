549c1cf4263a2ffa8d6a98e2a8c29e25941b4e84
Hal M. Spitz <hal.spitz@gmail.com>
2020-05-20 11:31:03 -0700

    just the usual routine maintenance

--- bash/Alias/db.sh
+++ bash/Alias/db.sh
@@ -1,4 +1,9 @@
-# alias pstart="postgres start -D /usr/local/var/postgres"
+# postgres
 alias pstart="pg_ctl start -D /usr/local/var/postgres"
 alias pstop="pg_ctl stop -D /usr/local/var/postgres"
 alias pstatus="pg_ctl status -D /usr/local/var/postgres"
+
+# mysql
+alias mstart="mysql.server start"
+alias mstop="mysql.server stop"
+
--- bash/Alias/instantlabs.sh
+++ bash/Alias/instantlabs.sh
@@ -1,9 +1,15 @@
 alias idb="cd ~/Devel/go/src/github.com/instantlabs/ildb; echo -ne '\033]1;ILDb\007' "
 alias idbb="cd ~/Devel/go/src/github.com/instantlabs/ildb/bbolt; echo -ne '\033]1;Bbolt\007' "
+alias idbdstress="cd ~/Devel/go/src/github.com/instantlabs/ildb/bbolt/cmd/stress_test; echo -ne '\033]1;StressTest\007' "
+
 alias ilib="cd ~/Devel/go/src/github.com/instantlabs/illib; echo -ne '\033]1;ILLib\007' "
+
 alias icore="cd ~/Devel/go/src/github.com/instantlabs/ilcore; echo -ne '\033]1;ILCore\007' "
+
 alias iclients="cd ~/Devel/go/src/github.com/instantlabs/clients; echo -ne '\033]1;IClients\007' "
-alias idbstress="cd ~/Devel/go/src/github.com/instantlabs/ildb/bbolt/cmd/stress_test; echo -ne '\033]1;StressTest\007' "
+
+alias irfc="cd ~/Devel/go/src/github.com/instantlabs/rfc; echo -ne '\033]1;RFC\007' "
+alias iilogger="cd ~/Devel/go/src/github.com/instantlabs/rfc/illogger; echo -ne '\033]1;RFC-illogger\007' "
 
 
 alias start-ilcore="icore; go build; env GO111MODULE=on go install github.com/instantlabs/ilcore/ilcore; ~/Devel/go/bin/ilcore"
