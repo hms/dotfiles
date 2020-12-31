652ad94ab70327495834e1ebf00bc33ab1368e21
Hal M. Spitz <hal.spitz@gmail.com>
2020-12-31 10:18:12 -0800

    cleaning up for my environment -- pulling in David's latest gitconfig

--- bash/Alias/git.sh
+++ bash/Alias/git.sh
@@ -1,1 +1,1 @@
-alias gd='git branch --merged | grep -v "master" | grep -v "*"| xargs git branch -d'
+alias gd='git branch --merged | grep -v "master" | grep -v "*"| xargs -I % sh "{ git branch -d -r origin/%; git branch -d % }"'
