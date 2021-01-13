9adfb3f3b26666efeb4d9e6a45c902985773e261
Hal M. Spitz <hal.spitz@gmail.com>
2021-01-13 10:14:05 -0800

    Clean up some scripts to account for ZSH vs. bash

--- bash/Alias/git.sh
+++ bash/Alias/git.sh
@@ -1,1 +1,4 @@
-alias gd='git branch --merged | grep -v "master" | grep -v "*"| xargs -I % sh "{ git branch -d -r origin/%; git branch -d % }"'
+alias gd-local='git branch --merged | grep -v "master" | grep -v "*"| xargs  git branch -d '
+alias gd-remote='git branch --merged | grep -v "master" | grep -v "*"| xargs -I % git branch -d -r origin/%'
+
+alias gd='gd-remote; gd-local'
