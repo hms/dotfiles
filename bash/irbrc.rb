34a943661e979e48c2485d20ddc86892924fc677
Hal M. Spitz <hal.spitz@gmail.com>
2022-01-04 14:13:19 -0800

    more cleanup of the irbrc logic

--- bash/irbrc.rb
+++ bash/irbrc.rb
@@ -13,16 +13,19 @@
 # ###########################################
 begin
   # Non-rails behavior goes here
-  return unless defined?(Rails) == 'constant'
+  return if defined?(Rails).nil?
 
-  unless defined?(DelayedJob) == 'constant'
-    class DelayedJob < ApplicationRecord; end
-  end
+  # add enough to query the DelayedJob queueing tables
+  ( class DelayedJob < ApplicationRecord; end ) if defined?(DelayedJob).nil?
 
-  # Add Rails version to the env and .rails_version (so we can add it to the prompt)
-  File.open('.rails-version', 'w') { |f| f.write(Rails.version) }
+  # Add the Rails version to the env
   ENV['RAILS_VERSION'] = Rails.version
+
+  # Add Rails version to .rails_version (so we can add it to the zsh prompt)
+  # but only if in the top level of a reop
+  File.open('.rails-version', 'w') { |f| f.write(Rails.version) } if Dir['.git'].present?
 rescue
+
 end
 
 def env
